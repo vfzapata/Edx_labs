711df856ecb76276b6fbd7e86b7873cb44558e3f
vfzapata <vfzapata@co.ibm.com>
2018-04-30 15:06:03 -0500

    Api github lab 1

--- edxLabUno/src/app/app.module.ts
+++ edxLabUno/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { GitSearchService } from './git-search.service';
 
 
 @NgModule({
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GitSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
